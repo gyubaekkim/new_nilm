0a9ade5318edbfa58bf80f2156652600d0f9d725
Gyubaek Kim <41098186+gyubaekkim@users.noreply.github.com>
2021-11-09 16:52:24 +0900

    Add files via upload

---
+++ work/benchmark/daedisaggregator_fe.py
@@ -0,0 +1,466 @@
+# Source code was originally from the following site
+#  - https://github.com/OdysseasKr/neural-disaggregator
+# We do not modify original source codes except for adding feature extracting method, "_feature_extract".
+# Some parts calling the method were also changed. Also, parameter setting for the changed input data was also affected.
+# Please refer to the source commentation
+
+
+from __future__ import print_function, division
+import random
+import sys
+
+from matplotlib import rcParams
+import matplotlib.pyplot as plt
+
+import pandas as pd
+import numpy as np
+import h5py
+
+from keras.models import load_model
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv1D, Reshape, Dropout
+from keras.utils.vis_utils import plot_model
+
+from nilmtk.utils import find_nearest
+from nilmtk.feature_detectors import cluster
+from nilmtk.legacy.disaggregate import Disaggregator
+from nilmtk.datastore import HDFDataStore
+
+class DAEDisaggregatorFE(Disaggregator):
+    '''Denoising Autoencoder disaggregator from Neural NILM
+    https://arxiv.org/pdf/1507.06594.pdf
+
+    Attributes
+    ----------
+    model : keras Sequential model
+    sequence_length : the size of window to use on the aggregate data
+    mmax : the maximum value of the aggregate data
+
+    MIN_CHUNK_LENGTH : int
+       the minimum length of an acceptable chunk
+    '''
+
+    def __init__(self, sequence_length):
+        '''Initialize disaggregator
+
+        Parameters
+        ----------
+        sequence_length : the size of window to use on the aggregate data
+        meter : a nilmtk.ElecMeter meter of the appliance to be disaggregated
+        '''
+        self.MODEL_NAME = "AUTOENCODER"
+        self.mmax = None
+        self.sequence_length = sequence_length
+        self.MIN_CHUNK_LENGTH = sequence_length
+        self.model = self._create_model(self.sequence_length)
+
+    def train(self, mains, meter, epochs=1, batch_size=16, **load_kwargs):
+        '''Train
+
+        Parameters
+        ----------
+        mains : a nilmtk.ElecMeter object for the aggregate data
+        meter : a nilmtk.ElecMeter object for the meter data
+        epochs : number of epochs to train
+        **load_kwargs : keyword arguments passed to `meter.power_series()`
+        '''
+
+        main_power_series = mains.power_series(**load_kwargs)
+        meter_power_series = meter.power_series(**load_kwargs)
+
+        # Train chunks
+        run = True
+        mainchunk = next(main_power_series)
+        meterchunk = next(meter_power_series)
+        if self.mmax == None:
+            self.mmax = mainchunk.max()
+
+        while(run):
+            mainchunk = self._normalize(mainchunk, self.mmax)
+            meterchunk = self._normalize(meterchunk, self.mmax)
+
+            self.train_on_chunk(mainchunk, meterchunk, epochs, batch_size)
+            try:
+                mainchunk = next(main_power_series)
+                meterchunk = next(meter_power_series)
+            except:
+                run = False
+
+    def train_on_chunk(self, mainchunk, meterchunk, epochs, batch_size):
+        '''Train using only one chunk
+
+        Parameters
+        ----------
+        mainchunk : chunk of site meter
+        meterchunk : chunk of appliance
+        epochs : number of epochs for training
+        '''
+
+        s = self.sequence_length
+        #up_limit =  min(len(mainchunk), len(meterchunk))
+        #down_limit =  max(len(mainchunk), len(meterchunk))
+
+        # Replace NaNs with 0s
+        mainchunk.fillna(0, inplace=True)
+        meterchunk.fillna(0, inplace=True)
+        ix = mainchunk.index.intersection(meterchunk.index)
+        mainchunk = mainchunk[ix]
+        meterchunk = meterchunk[ix]
+
+        # Create array of batches
+        #additional = s - ((up_limit-down_limit) % s)
+        additional = s - (len(ix) % s)
+        X_batch = np.append(mainchunk, np.zeros(additional))
+        Y_batch = np.append(meterchunk, np.zeros(additional))
+
+        #X_batch = np.reshape(X_batch, (int(len(X_batch) / s), s, 1))
+        X_batch = np.reshape(X_batch, (-1,1))       
+        feature_names, X_batch = self._feature_extract(X_batch)
+        print('feature selection is done..')    
+        X_batch = np.reshape(X_batch, (int(X_batch.shape[0] / s), s, X_batch.shape[1]))      
+        
+        
+        Y_batch = np.reshape(Y_batch, (int(len(Y_batch) / s), s, 1))
+
+        self.model.fit(X_batch, Y_batch, batch_size=batch_size, epochs=epochs, shuffle=True)
+
+    def train_across_buildings(self, mainlist, meterlist, epochs=1, batch_size=128, **load_kwargs):
+        assert len(mainlist) == len(meterlist), "Number of main and meter channels should be equal"
+        num_meters = len(mainlist)
+
+        mainps = [None] * num_meters
+        meterps = [None] * num_meters
+        mainchunks = [None] * num_meters
+        meterchunks = [None] * num_meters
+
+        for i,m in enumerate(mainlist):
+            mainps[i] = m.power_series(**load_kwargs)
+
+        for i,m in enumerate(meterlist):
+            meterps[i] = m.power_series(**load_kwargs)
+
+        for i in range(num_meters):
+            mainchunks[i] = next(mainps[i])
+            meterchunks[i] = next(meterps[i])
+        if self.mmax == None:
+            self.mmax = max([m.max() for m in mainchunks])
+
+
+        run = True
+        while(run):
+            mainchunks = [self._normalize(m, self.mmax) for m in mainchunks]
+            meterchunks = [self._normalize(m, self.mmax) for m in meterchunks]
+
+            self.train_across_buildings_chunk(mainchunks, meterchunks, epochs, batch_size)
+            try:
+                for i in range(num_meters):
+                    mainchunks[i] = next(mainps[i])
+                    meterchunks[i] = next(meterps[i])
+            except:
+                run = False
+
+    def train_across_buildings_chunk(self, mainchunks, meterchunks, epochs, batch_size):
+        num_meters = len(mainchunks)
+        batch_size = int(batch_size/num_meters)
+        num_of_batches = [None] * num_meters
+        s = self.sequence_length
+        for i in range(num_meters):
+            mainchunks[i].fillna(0, inplace=True)
+            meterchunks[i].fillna(0, inplace=True)
+            ix = mainchunks[i].index.intersection(meterchunks[i].index)
+            m1 = mainchunks[i]
+            m2 = meterchunks[i]
+            mainchunks[i] = m1[ix]
+            meterchunks[i] = m2[ix]
+
+            num_of_batches[i] = int(len(ix)/(s*batch_size)) - 1
+
+        for e in range(epochs):
+            print(e)
+            batch_indexes = list(range(min(num_of_batches)))
+            random.shuffle(batch_indexes)
+
+            for bi, b in enumerate(batch_indexes):
+
+                print("Batch {} of {}".format(bi,num_of_batches), end="\r")
+                sys.stdout.flush()
+                X_batch = np.empty((batch_size*num_meters, s, 1))
+                Y_batch = np.empty((batch_size*num_meters, s, 1))
+
+                for i in range(num_meters):
+                    mainpart = mainchunks[i]
+                    meterpart = meterchunks[i]
+                    mainpart = mainpart[b*batch_size*s:(b+1)*batch_size*s]
+                    meterpart = meterpart[b*batch_size*s:(b+1)*batch_size*s]
+                    X = np.reshape(mainpart, (batch_size, s, 1))
+                    Y = np.reshape(meterpart, (batch_size, s, 1))
+
+                    X_batch[i*batch_size:(i+1)*batch_size] = np.array(X)
+                    Y_batch[i*batch_size:(i+1)*batch_size] = np.array(Y)
+
+                p = np.random.permutation(len(X_batch))
+                X_batch, Y_batch = X_batch[p], Y_batch[p]
+
+                self.model.train_on_batch(X_batch, Y_batch)
+            print("\n")
+
+    def disaggregate(self, mains, output_datastore, meter_metadata, **load_kwargs):
+        '''Disaggregate mains according to the model learnt.
+
+        Parameters
+        ----------
+        mains : nilmtk.ElecMeter
+        output_datastore : instance of nilmtk.DataStore subclass
+            For storing power predictions from disaggregation algorithm.
+        meter_metadata : metadata for the produced output
+        **load_kwargs : key word arguments
+            Passed to `mains.power_series(**kwargs)`
+        '''
+
+        load_kwargs = self._pre_disaggregation_checks(load_kwargs)
+
+        load_kwargs.setdefault('sample_period', 60)
+        load_kwargs.setdefault('sections', mains.good_sections())
+
+        timeframes = []
+        building_path = '/building{}'.format(mains.building())
+        mains_data_location = building_path + '/elec/meter1'
+        data_is_available = False
+
+        for chunk in mains.power_series(**load_kwargs):
+            if len(chunk) < self.MIN_CHUNK_LENGTH:
+                continue
+            print("New sensible chunk: {}".format(len(chunk)))
+
+            timeframes.append(chunk.timeframe)
+            measurement = chunk.name
+            chunk2 = self._normalize(chunk, self.mmax)
+
+            appliance_power = self.disaggregate_chunk(chunk2)
+            appliance_power[appliance_power < 0] = 0
+            appliance_power = self._denormalize(appliance_power, self.mmax)
+
+            # Append prediction to output
+            data_is_available = True
+            cols = pd.MultiIndex.from_tuples([chunk.name])
+            meter_instance = meter_metadata.instance()
+            df = pd.DataFrame(
+                appliance_power.values, index=appliance_power.index,
+                columns=cols, dtype="float32")
+            key = '{}/elec/meter{}'.format(building_path, meter_instance)
+            output_datastore.append(key, df)
+
+            # Append aggregate data to output
+            mains_df = pd.DataFrame(chunk, columns=cols, dtype="float32")
+            output_datastore.append(key=mains_data_location, value=mains_df)
+
+        # Save metadata to output
+        if data_is_available:
+            self._save_metadata_for_disaggregation(
+                output_datastore=output_datastore,
+                sample_period=load_kwargs['sample_period'],
+                measurement=measurement,
+                timeframes=timeframes,
+                building=mains.building(),
+                meters=[meter_metadata]
+            )
+
+    def disaggregate_chunk(self, mains):
+        '''In-memory disaggregation.
+
+        Parameters
+        ----------
+        mains : pd.Series to disaggregate
+        Returns
+        -------
+        appliance_powers : pd.DataFrame where each column represents a
+            disaggregated appliance.  Column names are the integer index
+            into `self.model` for the appliance in question.
+        '''
+        s = self.sequence_length
+        up_limit = len(mains)
+
+        mains.fillna(0, inplace=True)
+
+        additional = s - (up_limit % s)
+        X_batch = np.append(mains, np.zeros(additional))
+        #X_batch = np.reshape(X_batch, (int(len(X_batch) / s), s ,1))
+        X_batch = np.reshape(X_batch, (-1,1))        
+        feature_names, X_batch = self._feature_extract(X_batch)        
+        X_batch = np.reshape(X_batch, (int(X_batch.shape[0] / s), s, X_batch.shape[1]))      
+
+        pred = self.model.predict(X_batch)
+        pred = np.reshape(pred, (up_limit + additional))[:up_limit]
+        column = pd.Series(pred, index=mains.index, name=0)
+
+        appliance_powers_dict = {}
+        appliance_powers_dict[0] = column
+        appliance_powers = pd.DataFrame(appliance_powers_dict)
+        return appliance_powers
+
+    def _feature_extract(self, active):
+       
+        feature_names = ['a0','a1','a2','a3','a4','a5','a6','a7','a8','a9',
+                         'f0','f1','f2','f3','f4','f5','f6','f7','f8','f9',
+                         'max','min','diff','mean','std','q0','q1','q2']
+            
+        n_features = len(feature_names)
+        n_size = active.size
+        
+        result = np.empty((n_size, n_features))
+        
+        for i in range(n_size):
+
+            try:                
+                window = []
+                val = []               
+                
+                a0 = active[0 if i < 9 else (i-9)][0]
+                a1 = active[0 if i < 8 else (i-8)][0]
+                a2 = active[0 if i < 7 else (i-7)][0]
+                a3 = active[0 if i < 6 else (i-6)][0]
+                a4 = active[0 if i < 5 else (i-5)][0]
+                a5 = active[0 if i < 4 else (i-4)][0]
+                a6 = active[0 if i < 3 else (i-3)][0]
+                a7 = active[0 if i < 2 else (i-2)][0]
+                a8 = active[0 if i < 1 else (i-1)][0]
+                a9 = active[i][0]
+               
+                # window
+                window.append(a0)
+                window.append(a1)
+                window.append(a2)
+                window.append(a3)
+                window.append(a4)
+                window.append(a5)
+                window.append(a6)
+                window.append(a7)
+                window.append(a8)
+                window.append(a9)                
+               
+                # active
+                val.append(window[0])
+                val.append(window[1])
+                val.append(window[2])
+                val.append(window[3])
+                val.append(window[4])
+                val.append(window[5])
+                val.append(window[6])
+                val.append(window[7])
+                val.append(window[8])
+                val.append(window[9])                
+ 
+                # fourier transform result (same length of original data)
+                f_data = np.array(np.fft.fft(window),dtype=float)     
+                val.append(f_data[0])
+                val.append(f_data[1])
+                val.append(f_data[2])
+                val.append(f_data[3])
+                val.append(f_data[4])
+                val.append(f_data[5])
+                val.append(f_data[6])
+                val.append(f_data[7])
+                val.append(f_data[8])
+                val.append(f_data[9])
+
+                # statistics 
+                val.append(max(window))
+                val.append(min(window))
+                val.append(max(window)-min(window))
+                val.append(np.mean(window))
+                val.append(np.std(window))
+                val.append(np.nanquantile(window,0.25))
+                val.append(np.nanquantile(window,0.5))
+                val.append(np.nanquantile(window,0.75))
+                
+                result[i] = val
+            
+            except Exception as e:
+                print('feature selection error', e)
+                continue
+        
+        return feature_names, result 
+
+    def import_model(self, filename):
+        '''Loads keras model from h5
+
+        Parameters
+        ----------
+        filename : filename for .h5 file
+
+        Returns: Keras model
+        '''
+        self.model = load_model(filename)
+        with h5py.File(filename, 'a') as hf:
+            ds = hf.get('disaggregator-data').get('mmax')
+            self.mmax = np.array(ds)[0]
+
+    def export_model(self, filename):
+        '''Saves keras model to h5
+
+        Parameters
+        ----------
+        filename : filename for .h5 file
+        '''
+        self.model.save(filename)
+        with h5py.File(filename, 'a') as hf:
+            gr = hf.create_group('disaggregator-data')
+            gr.create_dataset('mmax', data = [self.mmax])
+
+    def _normalize(self, chunk, mmax):
+        '''Normalizes timeseries
+
+        Parameters
+        ----------
+        chunk : the timeseries to normalize
+        max : max value of the powerseries
+
+        Returns: Normalized timeseries
+        '''
+        tchunk = chunk / mmax
+        return tchunk
+
+    def _denormalize(self, chunk, mmax):
+        '''Deormalizes timeseries
+        Note: This is not entirely correct
+
+        Parameters
+        ----------
+        chunk : the timeseries to denormalize
+        max : max value used for normalization
+
+        Returns: Denormalized timeseries
+        '''
+        tchunk = chunk * mmax
+        return tchunk
+
+    def _create_model(self, sequence_len):
+        '''Creates the Auto encoder module described in the paper
+        '''
+        model = Sequential()
+
+        # 1D Conv
+#        model.add(Conv1D(8, 4, activation="linear", input_shape=(sequence_len, 1), padding="same", strides=1))
+        model.add(Conv1D(8, 4, activation="linear", input_shape=(sequence_len, 28), padding="same", strides=1))
+        model.add(Flatten())
+
+        # Fully Connected Layers
+        model.add(Dropout(0.2))
+        model.add(Dense((sequence_len-0)*8, activation='relu'))
+
+        model.add(Dropout(0.2))
+        model.add(Dense(128, activation='relu'))
+
+        model.add(Dropout(0.2))
+        model.add(Dense((sequence_len-0)*8, activation='relu'))
+
+        model.add(Dropout(0.2))
+
+        # 1D Conv
+        model.add(Reshape(((sequence_len-0), 8)))
+        model.add(Conv1D(1, 4, activation="linear", padding="same", strides=1))
+
+        model.compile(loss='mse', optimizer='adam')
+        plot_model(model, to_file='model.png', show_shapes=True)
+
+        return model
